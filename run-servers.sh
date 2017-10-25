458ff39c7340e96c4cb686baf75167d239b8c213
tamal <tamal@appscode.com>
2017-10-25 12:38:58 -0700

    added-all

--- run-servers.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-kubectl run nginx --image=appscode/nginx-two-tcp:demo
-kubectl expose deployment nginx --name=web --port=80 --target-port=80
-
-kubectl run echoserver --image=gcr.io/google_containers/echoserver:1.4
-kubectl expose deployment echoserver --name=echo --port=443 --target-port=8080
